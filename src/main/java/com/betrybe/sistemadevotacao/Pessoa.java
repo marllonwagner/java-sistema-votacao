dc608e60a1578691e8f2360e01bce1a15463f4e1
marllonwagner <marllonwagner91@gmail.com>
2023-07-10 14:23:10 -0300

    task 1 javadoc add

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,12 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Classe abstrata que representa uma pessoa.
+ */
 public abstract class Pessoa {
+
   private String nome;
 
+  /**
+   * Obtém o nome da pessoa.
+   *
+   * @return O nome da pessoa.
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Define o nome da pessoa.
+   *
+   * @param nome O nome da pessoa.
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+private int numero;
+private int votos;
 
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
 }
