0faa7cf3a7237abb091bb54867eb80c67d9d2c88
marllonwagner <marllonwagner91@gmail.com>
2023-09-13 19:48:48 -0300

    .

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,5 +1,7 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
 /**
  * Classe responsável por executar a aplicação de votação.
  */
@@ -11,23 +13,72 @@ public class Principal {
    * @param args argumentos de linha de comando
    */
   public static void main(String[] args) {
-    GerenciamentoVotacao votacao = new GerenciamentoVotacao();
-    System.out.println("CADASTRO CANDIDATO");
-    System.out.println(votacao.pessoasCandidatas.size());
-    votacao.cadastrarPessoaCandidata("sunda", 20);
-    System.out.println(votacao.pessoasCandidatas.size());
-    votacao.cadastrarPessoaCandidata("birunda", 21);
-    System.out.println(votacao.pessoasCandidatas.size());
-    votacao.cadastrarPessoaCandidata("geronimo", 21);
-    System.out.println(votacao.pessoasCandidatas.size());
+    GerenciamentoVotacao eleicao = new GerenciamentoVotacao();
+    Scanner scanner = new Scanner(System.in);
 
-    votacao.cadastrarPessoaEleitora("jurandir", "123");
-    votacao.cadastrarPessoaEleitora("jurandir", "124");
+    int option = 0;
 
-    votacao.votar("123", 20);
-    votacao.votar("124", 21);
+    while (option != 2) {
+      System.out.println("""
+          Cadastrar pessoa candidata?
+          1 - Sim
+          2 - Não
+          Entre com o número correspondente à opção desejada:""");
+      option = scanner.nextInt();
 
-    votacao.mostrarResultado();
+      if (option == 1) {
+        scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String candName = scanner.nextLine();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int candNum = scanner.nextInt();
+        eleicao.cadastrarPessoaCandidata(candName, candNum);
+
+        int eleOption = 0;
+        while (eleOption != 2) {
+          System.out.println("""
+              Cadastrar pessoa eleitora?
+              1 - Sim
+              2 - Não
+              Entre com o número correspondente à opção desejada:""");
+          scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+          eleOption = scanner.nextInt();
+
+          if (eleOption == 1) {
+            scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+            System.out.println("Entre com o nome da pessoa eleitora:");
+            String eleName = scanner.nextLine();
+            System.out.println("Entre com o CPF da pessoa eleitora:");
+            String eleCpf = scanner.nextLine();
+            eleicao.cadastrarPessoaEleitora(eleName, eleCpf);
+          }
+        }
+      }
+    }
+
+    int menuOption = 0;
+    while (menuOption != 3) {
+      System.out.println("""
+          Entre com o número correspondente à opção desejada:
+          1 - Votar
+          2 - Resultado Parcial
+          3 - Finalizar Votação""");
+      menuOption = scanner.nextInt();
+
+      if (menuOption == 1) {
+        scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+        System.out.println("Entre com o CPF da pessoa eleitora:");
+        String cpfEleitora = scanner.nextLine();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int numeroCandidata = scanner.nextInt();
+        eleicao.votar(cpfEleitora, numeroCandidata);
+        System.out.println("Voto computado com sucesso!");
+      } else if (menuOption == 2) {
+        eleicao.mostrarResultado();
+      }
+    }
+
+    System.out.println("Resultado Final:");
+    eleicao.mostrarResultado();
   }
-
 }
